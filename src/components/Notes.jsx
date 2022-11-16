1d05f41ec4001ee30d15a381fbed43214a49eaf1
Jovit <jovitsuarez07@gmail.com>
2022-11-16 22:09:52 +0800

    Colorful Notes - First commit

---
+++ src/components/Notes.jsx
@@ -0,0 +1,14 @@
+function Notes() {
+  return (
+    <>
+        <section className="p-2">
+            <h1 className="m-0">Notes</h1>
+            <p>No saved notes yet.</p>
+        </section>
+        
+
+    </>
+  )
+}
+
+export default Notes
